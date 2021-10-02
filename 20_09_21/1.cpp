9c308f8dcd6d6e87a8d1be267ba3d679fbba9c87
Melques <57859163+Melques@users.noreply.github.com>
2021-10-02 23:06:48 +0300

    Загрузил файлы из домашней работы
    Номер задания указан в имени файла

---
+++ 20_09_21/1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int n;
+    cin >> n;
+    if (n > 99 && n < 1000) cout << "Число трехзначно";
+    else cout << "Число НЕ трёхзначно";
+}
+
---
+++ 20_09_21/2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int d, m, ans, k = 1;
+    while (1) {
+        cout << "Введите номер месяца: ";
+        cin >> m;
+        if (m > 0 && m < 13) break;
+        cout << "//Введённые данные некортектны" << endl;
+    }
+    while (1) {
+        cout << "Введите день: ";
+        cin >> d;
+        if (d > 0 && d < 32) {
+            if (m == 2 && d <= 28) break;
+            if (m < 8 && m != 2) {
+                if (m % 2 == 0 && d <= 30) break;
+                if (m % 2 == 1 && d <= 31) break;
+            }
+            if (m >= 8) {
+                if (m % 2 == 1 && d <= 30) break;
+                if (m % 2 == 1 && d <= 31) break;
+            }
+        }
+        cout << "//Введённые данные некортектны" << endl;
+    }
+    
+    
+    cout << "Дней для нового года: " << 365 + (m > 2) * 2 - (m < 9) * (31 * (m / 2) + 30 * ((m - 1) / 2)) - (m > 8) * (31 * ((m + 1) / 2)  + 30 * ((m - 2) / 2)) - d;
+}
