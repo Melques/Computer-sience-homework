4b75190cc89e7bf31f30f6791d1e2d5d89e2e982
Melques <57859163+Melques@users.noreply.github.com>
2021-10-21 19:14:43 +0300

    Add files via upload

---
+++ Task 3/3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+int F(int n) {
+    if (n <= 1) return 0;
+    for (int i = 2; i <= n; i++){
+        if (n % i == 0) {
+            cout << i << "  ";
+           
+            return  F(n / i);
+        }
+    }
+}
+int main()
+{
+    int N;
+    cin >> N;
+    if (N == 1) cout << 1;
+    else F(N);
+    return 0;
+}
+
